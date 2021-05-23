787221de9ea19a91fc0ba57e7cebc4daf3a43162
sai nikhil <sainikhildeekonda@gmail.com>
2021-05-23 12:52:42 -0400

    created api folder with three java classes
    api folder with all entities and services, adding serialization added

---
+++ app/src/main/java/com/example/finalprojectapi/api/service/WeatherMapService.java
@@ -0,0 +1,16 @@
+package com.example.finalprojectapi.api.service;
+
+
+//Kinda like our Dao
+
+import com.example.finalprojectapi.api.entity.WeatherLocation;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherMapService {
+
+    @GET("weather?appid=b33f168a4e526bcffb5878682d4e1d96")
+    Call<WeatherLocation> getWeather(@Query("q") String city);
+}
